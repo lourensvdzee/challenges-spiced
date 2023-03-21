e777439143e0661fc145f74fa1d3bece233185d5
Lourens-Spiced <lourensvdzee@gmail.com>
2023-03-21 01:22:50 +0100

    teenager check finished

--- js-conditions-and-booleans/js-conditions-and-booleans_teenager-check/js/index.js
+++ js-conditions-and-booleans/js-conditions-and-booleans_teenager-check/js/index.js
@@ -2,12 +2,28 @@ const input = document.querySelector('[data-js="age-input"]');
 const button = document.querySelector('[data-js="age-button"]');
 const output = document.querySelector('[data-js="age-output"]');
 
+
 button.addEventListener("click", () => {
   // Number(x) converts the string from the input to a number
   // Beware: input values are *always* strings!
   const age = Number(input.value);
 
+  //
+  //  if (Number < 18) {
+  //    console.log = "You are a teen";
+  //  } else {
+  //    console.log = "You are not a teen";
+  //  } 
+  
+
   // Exercise:
   // Use conditions and the logical AND operator to write
   // "You are a teen." or "You are not a teen." into the output.
+
+  if (age <= 17 && age >= 13) {
+    console.log("You are a teen.");
+  } else {
+    console.log("You are not a teen.");
+  }
 });
+
