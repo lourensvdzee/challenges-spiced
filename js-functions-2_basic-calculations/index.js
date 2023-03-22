b831775d9967d2c245a2dee2fb4353e8e560f383
Lourens-Spiced <lourensvdzee@gmail.com>
2023-03-22 11:13:54 +0100

    js-basic-challenges-greeting-page

--- js-functions-2_basic-calculations/index.js
+++ js-functions-2_basic-calculations/index.js
@@ -12,22 +12,32 @@ They should return the result of the respective calculation:
 
 function add(a, b) {
   // Code here
+  const sum = a+b;
+  return sum;
 }
 
 function subtract(a, b) {
   // Code here
+  const sum = a-b;
+  return sum;
 }
 
 function multiply(a, b) {
   // Code here
+  const sum = a*b;
+  return sum;
 }
 
 function divide(a, b) {
   // Code here
+  const sum = a/b;
+  return sum; 
 }
 
 function remainder(a, b) {
   // Code here
+  const sum = a%b;
+  return sum; 
 }
 
 /*
