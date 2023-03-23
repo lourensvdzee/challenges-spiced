9e78c1c258f981f11b1e4bc08a05b1cf7b264d8f
Lourens-Spiced <lourensvdzee@gmail.com>
2023-03-23 17:07:19 +0100

    commit work so far

--- js-forms_checkbox-input/js/index.js
+++ js-forms_checkbox-input/js/index.js
@@ -15,9 +15,33 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
+  const formElements = event.target.elements;
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+
+  console.log("data: ", data);
+
+  const tos = formElements.tos.checked;
+  console.log("tos: ", tos);
 
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
   alert("Form submitted");
 });
+
+//example
+// const form = document.querySelector('[data-js="form"]');
+
+// form.addEventListener("submit", (event) => {
+//     event.preventDefault();
+
+//     const formData = new FormData(event.target);
+//     const data = Object.fromEntries(formData);
+
+//     console.log(data);
+
+// });
+
+
+// Const checkbox = formelements.checkbox.checked;
