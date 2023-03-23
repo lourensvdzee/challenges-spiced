a6252989aa7b4f1a64ecdda76df8004bf2863792
Lourens-Spiced <lourensvdzee@gmail.com>
2023-03-23 22:54:54 +0100

    js forms checkbox input finished

--- js-forms_checkbox-input/js/index.js
+++ js-forms_checkbox-input/js/index.js
@@ -2,22 +2,34 @@ console.clear();
 
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
+const success = document.querySelector('[data-js="success" ]');
 
-function hideTosError() {
-  tosError.setAttribute("hidden", "");
+function hideMessage(message) {
+  message.setAttribute("hidden", "");
 }
 
-function showTosError() {
-  tosError.removeAttribute("hidden");
+function showMessage(message) {
+  message.removeAttribute("hidden");
 }
+
+hideMessage(success);
+hideMessage(tosError);
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
-
+  const elements = event.target.elements;
+  const ifCheckboxChecked = elements.tos.checked;
+  if (!ifCheckboxChecked) {
+    showMessage(tosError);
+    return;
+  } else {
+    showMessage(success);
+    hideMessage(tosError);
+    alert("Form submitted");
+  }
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
-  alert("Form submitted");
 });
