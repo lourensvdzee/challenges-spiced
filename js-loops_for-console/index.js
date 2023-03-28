62f98cd35ce6d034e2f02f23e980b2a9476eabe1
Lourens-Spiced <lourensvdzee@gmail.com>
2023-03-28 15:18:08 +0200

    js for loop and console finished

--- js-loops_for-console/index.js
+++ js-loops_for-console/index.js
@@ -1,1 +1,13 @@
 console.clear();
+/* 
+Log the numbers from 0 to 100 (including) into the console. Use a for loop.
+
+Make sure your for loop has
+
+- an internal counter variable
+- a criterion to be met by the counter variable
+- a rule how the counter variable is increased */
+
+for (let counter = 0; counter < 101; counter++) {
+    console.log(counter);
+}
