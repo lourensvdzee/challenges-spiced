7c282878feb3513148196ea3b41ab6f0bc768faa
Lourens-Spiced <lourensvdzee@gmail.com>
2023-03-29 17:00:46 +0200

    js array methods map finished

--- js-array-methods_map/index.js
+++ js-array-methods_map/index.js
@@ -24,10 +24,34 @@ const cards = [
   },
 ];
 
-const lowerCaseAnswers = null; // ['as often as you like.', ...]
+/* Task
+In the [`index.js`](./index.js) file, there is a `cards` array with three objects.
 
-const questionsAndAnswersTogether = null; // ["How often can I use <header>? - As often as you like.", ...]
+Below this array, you will find a couple of variables (e.g. `lowerCaseAnswers`). They all are initialized with `null`.
 
-const questionAndAnswer = null; // [{ question: 'How often can I use <header>?', answer: 'As often as you like.'}, {...}]
+And now it's your turn: delete the value `null` and write code such that the variables have a value corresponding to their name. Use `map` to achieve this goal.
+
+You will find some hints as comments.
+ */
+
+/* const lowerCaseAnswers = null; // ['as often as you like.', ...] */
+const lowerCaseAnswers = cards.map((card) => {
+  return card.answer.toLowerCase();
+}); // ['as often as you like.', ...]
+
+console.log(lowerCaseAnswers);
+
+/* const questionsAndAnswersTogether = null; // ["How often can I use <header>? - As often as you like.", ...]*/
+const questionsAndAnswersTogether = cards.map((card) => {
+  let result = card.question + "-" + card.answer;
+  return result;
+});
+console.log(questionsAndAnswersTogether);
+
+/* const questionAndAnswer = null; // [{ question: 'How often can I use <header>?', answer: 'As often as you like.'}, {...}]*/
+const questionAndAnswer = cards.map((card) => {
+  let result = "question: " + card.question + "-" + "answer: " + card.answer;
+  return result;
+});
 
 export { lowerCaseAnswers, questionsAndAnswersTogether, questionAndAnswer };
