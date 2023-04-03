e3e1c102d000411eb66f23aada9d711026ac1646
Lourens-Spiced <lourensvdzee@gmail.com>
2023-04-03 11:18:07 +0200

    init js array methods 2 news flash

---
+++ js-array-methods-2_news-flash/index.js
@@ -0,0 +1,21 @@
+import { news } from "./utils/news.js";
+import { Card } from "./components/Card/Card.js";
+import { checkFilteredNews, checkSortedNews } from "./utils/results.js";
+
+const container = document.querySelector('[data-js="card-container"]');
+
+// Part 1 - start here
+filteredNews = news;
+
+// Part 2 - start here
+const sortedNews = filteredNews;
+
+sortedNews.forEach((news) => {
+  const cardElement = Card(news);
+  container.append(cardElement);
+});
+
+// Check your filter and sorting order here:
+checkFilteredNews(filteredNews);
+
+checkSortedNews(sortedNews);
