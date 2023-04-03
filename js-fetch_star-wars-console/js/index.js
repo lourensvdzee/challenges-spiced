1ab54398893745569589c7de077ed80d30defd6f
Lourens-Spiced <lourensvdzee@gmail.com>
2023-04-03 16:06:20 +0200

    fetch star wars console finished

--- js-fetch_star-wars-console/js/index.js
+++ js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,55 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+// 1. returns entire data object
+/* function fetchData() {
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+        });
+} */
+
+// 2. returns different names of the data object
+
+/* function fetchData() {
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log("Data fetched:", data);
+            const names = data.results.map((person) => person.name);
+            console.log("Names extracted:", names);
+        })
+      .catch((error) => {
+         console.error("Error fetching data:", error);
+     }); 
+} */
+
+// 3. returns eye colors from R2-D2
+/* function fetchData() {
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log("Data fetched:", data);
+            const r2d2 = data.results.find((person) => person.name === "R2-D2");
+            console.log("R2-D2's eye color is:", r2d2.eye_color);
+        })
+        .catch((error) => {
+            console.error("Error fetching data:", error);
+        });
+} */
+
+// 4 using a try ... catch block
+async function fetchData() {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log("Data fetched:", data);
+        const names = data.results.map((person) => person.birth_year);
+        console.log("Names extracted:", names);
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
 
 fetchData();
