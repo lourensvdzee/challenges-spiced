8dc064e4e7266dcc10229c30007af529a8d86024
Lourens-Spiced <lourensvdzee@gmail.com>
2023-04-05 00:04:02 +0200

    fun with flags finished

--- js-array-methods-2_fun-with-flags/index.js
+++ js-array-methods-2_fun-with-flags/index.js
@@ -4,15 +4,39 @@ import { Country } from "./components/Country/Country.js";
 const container = document.querySelector('[data-js="card-container"]');
 const queryInput = document.querySelector('[data-js="query-input"]');
 
-queryInput.addEventListener("input", (event) => {
+//### part 1
+/* queryInput.addEventListener("input", (event) => {
+  // console.log('event happened!');
   container.innerHTML = "";
 
   const searchString = event.target.value;
 
-  const foundCountry = null;
+  const foundCountry = countries.find((country) =>
+    country.name.startsWith(searchString.toLowerCase));
 
   if (foundCountry) {
+    console.log("country found: ", foundCountry.name);
     const countryElement = Country(foundCountry);
     container.append(countryElement);
   }
+}); */
+
+//### part 2
+queryInput.addEventListener("input", (event) => {
+  container.innerHTML = "";
+
+  const searchString = event.target.value.toLowerCase();
+
+  const foundCountries = countries.filter((country) => {
+    return country.name.toLowerCase().startsWith(searchString);
+  });
+
+  console.clear();
+  foundCountries.forEach((country) => {
+    if (country) {
+      console.log(country);
+      const countryElement = Country(country);
+      container.append(countryElement);
+    }
+  });
 });
