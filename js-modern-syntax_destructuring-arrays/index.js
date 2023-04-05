eb014737abe49ebd2147b0d6f9722849a0e70f32
Lourens-Spiced <lourensvdzee@gmail.com>
2023-04-05 12:00:19 +0200

    destructuring arrays finished

--- js-modern-syntax_destructuring-arrays/index.js
+++ js-modern-syntax_destructuring-arrays/index.js
@@ -6,6 +6,9 @@ console.clear();
 
 const item = ["Egg", 0.25, 12];
 
+const [name, price, quantity] = item;
+console.log(name, price, quantity);
+
 // example: export const [value1, value2] = array;
 
 // EXERCISE 2
@@ -13,11 +16,17 @@ const item = ["Egg", 0.25, 12];
 
 const personChris = [12, "Chris", "Owen"];
 
+const [ageOfChris, firstNameOfChris, lastNameOfChris,] = personChris;
+console.log(ageOfChris, firstNameOfChris, lastNameOfChris);
+
 // EXERCISE 3
 // Use array destructuring to extract the variables "firstNameOfAlex" and "lastNameOfAlex".
 // Make sure not to have unused variables.
 
 const personAlex = ["Alex", 12, "Main"];
+
+const [firstNameOfAlex, , lastNameOfAlex] = personAlex;
+console.log(firstNameOfAlex, lastNameOfAlex);
 
 // EXERCISE 4
 // Use array destructuring to extract the last name from the array
@@ -25,11 +34,16 @@ const personAlex = ["Alex", 12, "Main"];
 
 const students = ["Christina", "Jon", "Alexandare"];
 
+const [, , lastName] = students;
+console.log(lastName);
+
 // EXERCISE 5
 // Use array destructuring to extract all of the names from this nested array.
 // Assign the given order of students to variables called "student1" to "student5".
 
 const nestedStudents = ["Chris", ["Ahmad", "Antigoni"], ["Toby", "Sam"]];
+const [student1, [student2, student3], [student4, student5]] = nestedStudents;
+console.log(student1, student2, student3, student4, student5);
 
 // EXERCISE 6
 // Spread the values of the values array into the function call of the add function.
@@ -37,10 +51,11 @@ const nestedStudents = ["Chris", ["Ahmad", "Antigoni"], ["Toby", "Sam"]];
 
 const values = [1, 6, 7, 9, 12, 5, 4];
 
-const result = add(1); // Spread values inside this function call
+const result = add(...values); // Spread values inside this function call
 
 function add(...values) {
   return values.reduce(
     (previousValue, currentValue) => previousValue + currentValue
   );
 }
+console.log(result);
