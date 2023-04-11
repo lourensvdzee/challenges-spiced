a6bf5bc68f0722e3fb76cfb1fb03e583f08c2080
Lourens-Spiced <lourensvdzee@gmail.com>
2023-04-11 19:08:24 +0200

    hello world article finished

--- react-basics_hello-world-article/src/App.js
+++ react-basics_hello-world-article/src/App.js
@@ -1,6 +1,18 @@
 import React from "react";
 import "./styles.css";
 
+//first return article
+//then return something...
+
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return <HelloWorldArticle />;
+}
+
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>Such a great headline</h1>
+      <p>With an amazing paragraph</p>
+    </article>
+  )
 }
