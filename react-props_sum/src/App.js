056c9b4232bbdcef3128ece7a1c567afd638ef35
Lourens-Spiced <lourensvdzee@gmail.com>
2023-04-12 11:51:35 +0200

    finished props sum

--- react-props_sum/src/App.js
+++ react-props_sum/src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  // return <h1>Replace me with your component!</h1>;
+  const valueA = 2;
+  const valueB = 3;
+
+  return (
+    <Sum valueA={valueA} valueB={valueB} />
+  );
+}
+
+function Sum({ valueA, valueB }) {
+  const sum = parseInt(valueA) + parseInt(valueB);
+
+  return (
+    <h1>{valueA} + {valueB} = {sum}</h1>
+  );
 }
