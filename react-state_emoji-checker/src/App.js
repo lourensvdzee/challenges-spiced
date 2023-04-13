a5c70226501dc8ea3151c3f73cac746066fe70c0
Lourens-Spiced <lourensvdzee@gmail.com>
2023-04-13 18:45:43 +0200

    state emoji checker finished

--- react-state_emoji-checker/src/App.js
+++ react-state_emoji-checker/src/App.js
@@ -1,56 +1,70 @@
 import React from "react";
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
-  let code = "?";
+  // let code = "?";
+  const [code, setCode] = useState("?");
 
   const validCode = "🐡🐠🐋";
+
+  const handleClick = (e) => {
+    console.log(e.target.innerText);
+    setCode(code + e.target.innerText);
+  };
+
+  function resetCode() {
+    setCode("");
+  }
 
   return (
     <div className="container">
       <div className="button-container">
-        <button
+        {/* <button
           type="button"
           onClick={() => {
             console.log("Update Code!");
           }}
-        >
+        > */}
+        <button type="button" onClick={handleClick}>
           <span role="img" aria-label="Pufferfish">
             🐡
           </span>
         </button>
-        <button
+        {/* <button
           type="button"
           onClick={() => {
             console.log("Update Code!");
           }}
-        >
+        > */}
+        <button type="button" onClick={handleClick}>
           <span role="img" aria-label="Whale">
             🐋
           </span>
         </button>
-        <button
+        {/* <button
           type="button"
           onClick={() => {
             console.log("Update Code!");
           }}
-        >
+        > */}
+        <button type="button" onClick={handleClick}>
           <span role="img" aria-label="Clownfish">
             🐠
           </span>
         </button>{" "}
       </div>
 
-      <button
+      {/* <button
         type="button"
         onClick={() => {
           console.log("Reset Code!");
         }}
-      >
+      > */}
+      <button type="button" onClick={resetCode}>
         Reset
       </button>
       <h2>{code}</h2>
-
       {code === validCode && <p>Valid code!</p>}
     </div>
   );
