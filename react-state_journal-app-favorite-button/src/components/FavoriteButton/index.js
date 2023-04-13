846d1a519872e62019d461d9aab49a211b1abbb9
Lourens-Spiced <lourensvdzee@gmail.com>
2023-04-13 17:13:17 +0200

    journal app fav button finished

--- react-state_journal-app-favorite-button/src/components/FavoriteButton/index.js
+++ react-state_journal-app-favorite-button/src/components/FavoriteButton/index.js
@@ -1,16 +1,22 @@
 import "./FavoriteButton.css";
 import { ReactComponent as StarFilled } from "./star-filled.svg";
 import { ReactComponent as Star } from "./star.svg";
+//import useState
+import { useState } from "react";
+
 
 export default function FavoriteButton() {
-  // this should be a state variable
-  const isFavorite = false;
+  // const isFavorite = false;
+  // let's add a the toggle function with useState, calling a new function called setIsFavorite as opposite of isFavorite
+  const [isFavorite, setIsFavorite] = useState(false);
 
   return (
     <button
       className="favorite-button"
       onClick={() => {
-        console.log("favorite button clicked");
+        // console.log("favorite button clicked");
+        //here we set the function for the toggle to work. Clicking means the opposite image is used. 
+        setIsFavorite(!isFavorite);
       }}
       aria-label="favorite"
     >
