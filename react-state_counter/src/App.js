de485a5e221d63bce4acbf5af8d660e480cf4eb9
Lourens-Spiced <lourensvdzee@gmail.com>
2023-04-13 16:37:39 +0200

    state counter finished

--- react-state_counter/src/App.js
+++ react-state_counter/src/App.js
@@ -1,8 +1,12 @@
+//imported useState from react library..
 import React from "react";
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  let count = 0;
+  //switched off let, as react can't handle it. Instead, we need a new function that react can read. We start counting from 0, so that is the initial variable
+  // let count = 0; 
+  const [count, setCount] = useState(0);
 
   return (
     <div className="container">
@@ -11,7 +15,9 @@ export default function App() {
         <button
           type="button"
           onClick={() => {
-            console.log("🤔");
+            //switched off emoji and added count -1 for button 1
+            //console.log("🤔");
+            setCount(count - 1);
           }}
         >
           -
@@ -19,7 +25,9 @@ export default function App() {
         <button
           type="button"
           onClick={() => {
-            console.log("🤔");
+            //switched off emoji and added count +1 for button 2
+            //console.log("🤔");
+            setCount(count + 1);
           }}
         >
           +
