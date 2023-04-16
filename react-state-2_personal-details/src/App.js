876382a761276d8dd7e9ecad93d06c9a5ef92eaf
Lourens-Spiced <lourensvdzee@gmail.com>
2023-04-17 00:39:00 +0200

    personal details #1 finished

--- react-state-2_personal-details/src/App.js
+++ react-state-2_personal-details/src/App.js
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import Form from "./components/Form.js";
 
 export default function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
   return (
     <div className="container">
       <h1>Personal Details Form</h1>
-      <Form />
+      <Form setName={setName} setEmail={setEmail} />
       <h2>Your submitted details:</h2>
       <p>
-        Name: <span className="output">John doe</span>
+        Name: <span className="output">{name}</span>
       </p>
       <p>
-        Email: <span className="output">John@Doe.com</span>
+        Email: <span className="output">{email}</span>
       </p>
     </div>
   );
--- react-state-2_personal-details/src/components/Form.js
+++ react-state-2_personal-details/src/components/Form.js
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import React from "react";
 
-export default function Form() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-
+export default function Form(props) {
   function handleSubmit(event) {
     event.preventDefault();
+    const newName = event.target.elements.name.value;
+    const newEmail = event.target.elements.email.value;
+    props.setName(newName);
+    props.setEmail(newEmail);
+    event.target.reset();
   }
 
   return (
