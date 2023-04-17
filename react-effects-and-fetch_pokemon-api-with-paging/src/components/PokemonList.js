74e905605bf96d462615efca268d699b81ac223f
Lourens-Spiced <lourensvdzee@gmail.com>
2023-04-17 17:49:23 +0200

    pokemon paging next page finished

--- react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.js
+++ react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.js
@@ -1,30 +1,34 @@
 import { useEffect, useState } from "react";
 
 export default function PokemonList() {
-  const [pokemon, setPokemon] = useState([]);
+  const [pokemon, setPokemon] = useState({});
+  const [nextUrl, setNextUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon?offset=0");
 
   useEffect(() => {
     async function loadPokemon() {
       try {
-        const response = await fetch(
-          "https://pokeapi.co/api/v2/pokemon?offset=0"
-        );
+        const response = await fetch(nextUrl);
         const data = await response.json();
-        setPokemon(data.results);
+        setPokemon(data);
       } catch (error) {
         console.log(error);
       }
     }
 
     loadPokemon();
-  }, []);
+  }, [nextUrl]);
+
+  const nextPage = () => {
+    setNextUrl(pokemon.next);
+  };
 
   return (
     <main>
       <button type="button">Previous Page</button>
-      <button type="button">Next Page</button>
+      <button onClick={nextPage} type="button">Next Page</button>
       <ul>
-        {pokemon.map(({ name }) => (
+        {pokemon?.results?.map(({ name }) => (
           <li key={name}>{name}</li>
         ))}
       </ul>
