61bbd2ecfb6f2104ca84ce494a0e5f51ac740789
Lourens-Spiced <lourensvdzee@gmail.com>
2023-04-24 14:33:10 +0200

    react component testing finished

--- react-component-testing_scorekeeper/components/Navigation/Navigation.test.js
+++ react-component-testing_scorekeeper/components/Navigation/Navigation.test.js
@@ -3,8 +3,16 @@ import Navigation from ".";
 
 jest.mock("next/router", () => ({
   useRouter() {
-    return { pathname: jest.fn() };
+    return { pathname: "/" };
   },
 }));
 
-test("renders with two links 'Play' and 'History'", () => {});
+test("renders with two links 'Play' and 'History'", () => {
+  render(<Navigation players={[]} />);
+
+  const playLink = screen.getByText("Play");
+  const historyLink = screen.getByText("History");
+
+  expect(playLink).toBeInTheDocument();
+  expect(historyLink).toBeInTheDocument();
+});
