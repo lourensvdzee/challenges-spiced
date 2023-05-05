0939dd64a8e9e474e2c9a6d5844c2e2410ae1f63
Lourens-Spiced <lourensvdzee@gmail.com>
2023-05-05 18:00:18 +0200

    backend api routes random character finished

--- backend-api-routes_random-character/pages/index.js
+++ backend-api-routes_random-character/pages/index.js
@@ -1,3 +1,20 @@
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const { data, error } = useSWR("/api/random-character", fetcher);
+
+  if (error) return <div>Failed to load data</div>;
+  if (!data) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <p>First Name: {data.firstName}</p>
+      <p>Last Name: {data.lastName}</p>
+      <p>Twitter Name: {data.twitter}</p>
+      <p>Geohash: {data.geohash}</p>
+      {/* add more keys here as desired */}
+    </div>
+  );
 }
