440c8d5ec217a4d974b9246cdbc0258b7086fded
Lourens-Spiced <lourensvdzee@gmail.com>
2023-05-08 10:53:16 +0200

    [id].js fixed too

--- backend-read_products/pages/api/products/[id].js
+++ backend-read_products/pages/api/products/[id].js
@@ -1,9 +1,13 @@
-import { products } from "../../../lib/products";
+import dbConnect from "../../../db/connect";
+import Product from "../../../db/models/Product";
+// import { products } from "../../../lib/products";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
+  await dbConnect();
+
   const { id } = request.query;
 
-  const product = products.find((product) => product.id === id);
+  const product = await Product.findById(id);
 
   if (!product) {
     return response.status(404).json({ status: "Not Found" });
@@ -11,3 +15,4 @@ export default function handler(request, response) {
 
   response.status(200).json(product);
 }
+
